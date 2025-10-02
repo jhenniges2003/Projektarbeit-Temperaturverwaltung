56011239b877e099e705d5912c1b885887c88adb
Thorben Hoffmann <thorben@MacBook-Air-von-Thorben.local>
2025-10-02 11:06:29 +0200

    change login delete sign in

--- temperaturverwaltung/resources/views/livewire/auth/login.blade.php
+++ temperaturverwaltung/resources/views/livewire/auth/login.blade.php
@@ -101,7 +101,7 @@ new #[Layout('components.layouts.auth')] class extends Component {
 }; ?>
 
 <div class="flex flex-col gap-6">
-    <x-auth-header :title="__('Log in to your account')" :description="__('Enter your email and password below to log in')" />
+    <x-auth-header :title="__('Log in to your account')" :description="__('Enter your username and password below to log in')" />
 
     <!-- Session Status -->
     <x-auth-session-status class="text-center" :status="session('status')" />
@@ -145,10 +145,5 @@ new #[Layout('components.layouts.auth')] class extends Component {
         </div>
     </form>
 
-    @if (Route::has('register'))
-        <div class="space-x-1 text-sm text-center rtl:space-x-reverse text-zinc-600 dark:text-zinc-400">
-            <span>{{ __('Don\'t have an account?') }}</span>
-            <flux:link :href="route('register')" wire:navigate>{{ __('Sign up') }}</flux:link>
-        </div>
-    @endif
+
 </div>
